b58b57b434840ff6ff7b4dd058781acd0f664be0
Lars Marius Ulsnes (Stasjonær) <lm@tigne.no>
2026-03-29 13:19:03 +0200

    initial spring scaffold

---
+++ src/main/java/com/lmuls/dealtracker/DealtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.lmuls.dealtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealtrackerApplication.class, args);
+	}
+
+}
