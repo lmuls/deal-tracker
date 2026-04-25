38735797157b5b4a8019145a7a42137cb0150181
Lars Marius Ulsnes (Stasjonær) <lm@tigne.no>
2026-04-25 09:15:54 +0200

    Fix bug infinite recursion

--- deal-tracker-webapp/src/main/java/com/lmuls/dealtracker/controller/SpaController.java
+++ deal-tracker-webapp/src/main/java/com/lmuls/dealtracker/controller/SpaController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SpaController {
 
-    @RequestMapping(value = {"/{path:[^\\.]*}", "/{path:[^\\.]*}/**"})
+    @RequestMapping(value = {"/{path:^(?!api|assets|static)[^\\.]*$}", "/{path:^(?!api|assets|static)[^\\.]*$}/**"})
     public String forward() {
         return "forward:/index.html";
     }
